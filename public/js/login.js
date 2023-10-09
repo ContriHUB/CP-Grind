68af05f26578670fc26fdb01caddc9efc9593cba
Mangal Gupta <127006680+mangalgithub@users.noreply.github.com>
2023-10-09 20:33:42 +0530

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,83 @@
+const passwordInput = document.getElementById("password");
+const cpasswordInput = document.getElementsByName("cpassword")[0];
+const showPasswordButton = document.getElementById("show-password");
+const hidePasswordButton = document.getElementById("hide-password");
+const showCPasswordButton = document.getElementById("show-cpassword");
+const hideCPasswordButton = document.getElementById("hide-cpassword");
+
+showPasswordButton.addEventListener("click", () => {
+  passwordInput.type = "text";
+  showPasswordButton.style.display = "none";
+  hidePasswordButton.style.display = "inline-block";
+});
+
+hidePasswordButton.addEventListener("click", () => {
+  passwordInput.type = "password";
+  showPasswordButton.style.display = "inline-block";
+  hidePasswordButton.style.display = "none";
+});
+
+showCPasswordButton.addEventListener("click", () => {
+  cpasswordInput.type = "text";
+  showCPasswordButton.style.display = "none";
+  hideCPasswordButton.style.display = "inline-block";
+});
+
+hideCPasswordButton.addEventListener("click", () => {
+  cpasswordInput.type = "password";
+  showCPasswordButton.style.display = "inline-block";
+  hideCPasswordButton.style.display = "none";
+});
+
+        function checkPasswordStrength() {
+          const passwordInput = document.getElementById("password");
+          const strengthIndicator = document.getElementById(
+            "password-strength-indicator"
+          );
+          const passwordStrengthMeter = document.getElementById(
+            "progress-strength-meter"
+          );
+          const password = passwordInput.value;
+
+          const result = zxcvbn(password);
+
+          // Set the color indicator and message based on password strength score
+       
+          switch (result.score) {
+            case 0:
+              strengthIndicator.innerHTML = "Very Weak";
+              passwordStrengthMeter.style.backgroundColor = "red";
+              break;
+            case 1:
+              strengthIndicator.innerHTML = "Weak";
+             passwordStrengthMeter.style.backgroundColor = "orange";
+              break;
+            case 2:
+              strengthIndicator.innerHTML = "Fair";
+              passwordStrengthMeter.style.backgroundColor = "yellow";
+              break;
+            case 3:
+              strengthIndicator.innerHTML = "Strong";
+             passwordStrengthMeter.style.backgroundColor = "green";
+              break;
+            case 4:
+              strengthIndicator.innerHTML = "Very Strong";
+             passwordStrengthMeter.style.backgroundColor = "darkgreen";
+              break;
+            default:
+              strengthIndicator.innerHTML = "";
+          }
+
+          // Update the password strength meter (progress bar)
+          const passwordStrengthScore = result.score + 1; // Scores are 0-based, so add 1
+          const meterWidth = (passwordStrengthScore / 5) * 100; // Convert to percentage
+          passwordStrengthMeter.style.width = `${meterWidth}%`;
+          passwordStrengthMeter.setAttribute(
+            "aria-valuenow",
+            passwordStrengthScore * 20
+          ); // Each score is worth 20
+        }
+
 let cnt = 0;
 function toggleit(){
     const signup = document.getElementById("sign_up");
