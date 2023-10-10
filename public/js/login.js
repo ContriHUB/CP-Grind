4767de552a5d7bac55c44475be38620e01c00000
dhanrajchaurasia <mnnitdhanraj@gmail.com>
2023-10-10 12:43:58 +0530

    fixed some stuff

--- public/js/login.js
+++ public/js/login.js
@@ -5,96 +5,89 @@ const hidePasswordButton = document.getElementById("hide-password");
 const showCPasswordButton = document.getElementById("show-cpassword");
 const hideCPasswordButton = document.getElementById("hide-cpassword");
 const passwordStrengthMessage = document.getElementById("password-strength-message");
-
-showPasswordButton.addEventListener("click", () => {
+function showPass(){
   passwordInput.type = "text";
   showPasswordButton.style.display = "none";
   hidePasswordButton.style.display = "inline-block";
-});
-
-hidePasswordButton.addEventListener("click", () => {
+};
+function hidePass(){
   passwordInput.type = "password";
   showPasswordButton.style.display = "inline-block";
   hidePasswordButton.style.display = "none";
-});
-
-showCPasswordButton.addEventListener("click", () => {
+};
+function showCPass(){
   cpasswordInput.type = "text";
   showCPasswordButton.style.display = "none";
   hideCPasswordButton.style.display = "inline-block";
-});
-
-hideCPasswordButton.addEventListener("click", () => {
+}
+function hideCPass(){
   cpasswordInput.type = "password";
   showCPasswordButton.style.display = "inline-block";
   hideCPasswordButton.style.display = "none";
-});
+}
 
-        function checkPasswordStrength() {
-          const passwordInput = document.getElementById("password");
-          const strengthIndicator = document.getElementById(
-            "password-strength-indicator"
-          );
-          const passwordStrengthMeter = document.getElementById(
-            "progress-strength-meter"
-          );
-          const password = passwordInput.value;
+// Set up the keyup event listener for password strength
+passwordInput.addEventListener("keyup", updatePasswordStrength);
 
-          const result = zxcvbn(password);
+function updatePasswordStrength() {
+  const strengthIndicator = document.getElementById("password-strength-indicator");
+  const passwordStrengthMeter = document.getElementById("progress-strength-meter");
+  const password = passwordInput.value;
+  const result = zxcvbn(password);
 
-          // Set the color indicator and message based on password strength score
-       
-          switch (result.score) {
-            case 0:
-              strengthIndicator.innerHTML = "Very Weak";
-              passwordStrengthMeter.style.display="block";
-              passwordStrengthMeter.style.backgroundColor = "red";
-               passwordStrengthMessage.innerHTML = "Very Weak Password";
-               passwordStrengthMessage.style.color = "red";
-              break;
-            case 1:
-              strengthIndicator.innerHTML = "Weak";
-             passwordStrengthMeter.style.backgroundColor = "orange";
-               passwordStrengthMessage.innerHTML = "Weak Password";
-               passwordStrengthMessage.style.color = "orange";
-              break;
-            case 2:
-              strengthIndicator.innerHTML = "Fair";
-              passwordStrengthMeter.style.backgroundColor = "yellow";
-                passwordStrengthMessage.innerHTML = "Fair Password";
-                passwordStrengthMessage.style.color = "yellow";
-              break;
-            case 3:
-              strengthIndicator.innerHTML = "Strong";
-             passwordStrengthMeter.style.backgroundColor = "green";
-               passwordStrengthMessage.innerHTML = "Strong Password";
-               passwordStrengthMessage.style.color = "green";
-              break;
-            case 4:
-              strengthIndicator.innerHTML = "Very Strong";
-             passwordStrengthMeter.style.backgroundColor = "darkgreen";
-               passwordStrengthMessage.innerHTML = "Very Strong Password";
-               passwordStrengthMessage.style.color = "darkgreen";
-              break;
-            default:
-              strengthIndicator.innerHTML = "";
-          }
+  // Set the color indicator and message based on password strength score
+  switch (result.score) {
+    case 0:
+      strengthIndicator.innerHTML = "Very Weak";
+      passwordStrengthMeter.style.display = "block";
+      passwordStrengthMeter.style.backgroundColor = "red";
+      passwordStrengthMessage.innerHTML = "Very Weak Password";
+      passwordStrengthMessage.style.color = "red";
+      break;
+    case 1:
+      strengthIndicator.innerHTML = "Weak";
+      passwordStrengthMeter.style.backgroundColor = "orange";
+      passwordStrengthMessage.innerHTML = "Weak Password";
+      passwordStrengthMessage.style.color = "orange";
+      break;
+    case 2:
+      strengthIndicator.innerHTML = "Fair";
+      passwordStrengthMeter.style.backgroundColor = "yellow";
+      passwordStrengthMessage.innerHTML = "Fair Password";
+      passwordStrengthMessage.style.color = "yellow";
+      break;
+    case 3:
+      strengthIndicator.innerHTML = "Strong";
+      passwordStrengthMeter.style.backgroundColor = "green";
+      passwordStrengthMessage.innerHTML = "Strong Password";
+      passwordStrengthMessage.style.color = "green";
+      break;
+    case 4:
+      strengthIndicator.innerHTML = "Very Strong";
+      passwordStrengthMeter.style.backgroundColor = "darkgreen";
+      passwordStrengthMessage.innerHTML = "Very Strong Password";
+      passwordStrengthMessage.style.color = "darkgreen";
+      break;
+    default:
+      strengthIndicator.innerHTML = "";
+  }
 
-          // Update the password strength meter (progress bar)
-          const passwordStrengthScore = result.score + 1; // Scores are 0-based, so add 1
-          const meterWidth = (passwordStrengthScore / 5) * 100; // Convert to percentage
-          passwordStrengthMeter.style.width = `${meterWidth}%`;
-          passwordStrengthMeter.setAttribute(
-            "aria-valuenow",
-            passwordStrengthScore * 20
-          ); // Each score is worth 20
+  // Update the password strength meter (progress bar)
+  const passwordStrengthScore = result.score + 1; // Scores are 0-based, so add 1
+  const meterWidth = (passwordStrengthScore / 5) * 100; // Convert to percentage
+  passwordStrengthMeter.style.width = `${meterWidth}%`;
+  passwordStrengthMeter.setAttribute(
+    "aria-valuenow",
+    passwordStrengthScore * 20
+  ); // Each score is worth 20
 
-           if (result.score >= 0) {
-             passwordStrengthMessage.style.display = "block";
-           } else {
-             passwordStrengthMessage.style.display = "none";
-           }
-        }
+  if (result.score >= 0) {
+    passwordStrengthMessage.style.display = "block";
+  } else {
+    passwordStrengthMessage.style.display = "none";
+  }
+}
+
 
 let cnt = 0;
 function toggleit(){
